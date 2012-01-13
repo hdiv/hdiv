c5532cfbffd93006caf2b7a2833475a58b2c57a2
Gotzon Illarramendi <gotzon@hdiv.org>
2012-01-13 16:30:32 +0100

    License in test

--- hdiv-core/src/test/java/org/hdiv/idGenerator/UidGeneratorTest.java
+++ hdiv-core/src/test/java/org/hdiv/idGenerator/UidGeneratorTest.java
@@ -1,49 +1,36 @@
+/*
+ * Copyright 2004-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hdiv.idGenerator;
 
-//import java.util.Random;
-//import java.util.UUID;
+import java.io.Serializable;
 
-import junit.framework.TestCase;
+import org.hdiv.AbstractHDIVTestCase;
 
-public class UidGeneratorTest extends TestCase{
+public class UidGeneratorTest extends AbstractHDIVTestCase {
 
-	public void testUidGenerationPerformance() {
+	private UidGenerator uidGenerator;
 
-//Only in Java 1.5
-//		{
-//			UUID id = UUID.randomUUID();
-//			String sId = id.toString();
-//			
-//			UidGenerator iudg = new RandomGuidUidGenerator();
-//			String gid = iudg.generateUid().toString();
-//			
-//			Random random = new Random(System.nanoTime() + this.hashCode());
-//			String rId = Long.valueOf(random.nextLong()).toString();
-//		}
-//		
-//		long time1 = System.nanoTime();
-//		
-//		UUID id = UUID.randomUUID();
-//		String sId = id.toString();
-//		
-//		long time2 = System.nanoTime();
-//		
-//		UidGenerator iudg = new RandomGuidUidGenerator();
-//		String gid = iudg.generateUid().toString();
-//		
-//		long time3 = System.nanoTime();
-//		
-//		Random random = new Random(System.nanoTime() + this.hashCode());
-//		String rId = Long.valueOf(random.nextLong()).toString();
-//		
-//		long time4 = System.nanoTime();
-//		
-//		System.out.println(sId);
-//		System.out.println(gid);
-//		System.out.println(rId);
-//		
-//		System.out.println(time2-time1);
-//		System.out.println(time3-time2);
-//		System.out.println(time4-time3);
+	protected void onSetUp() throws Exception {
+		this.uidGenerator = (UidGenerator) super.getApplicationContext().getBean(UidGenerator.class);
 	}
+
+	public void testUidGeneration() {
+		Serializable id = this.uidGenerator.generateUid();
+		assertNotNull(id);
+
+	}
+
 }
