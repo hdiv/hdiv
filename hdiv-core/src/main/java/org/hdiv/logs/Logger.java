5b2778897a7759175e4322500e170b0d2e4e0261
gillarramendi <gotzon@hdiv.org>
2013-07-30 12:08:52 +0200

    More extensible Logger

--- hdiv-core/src/main/java/org/hdiv/logs/Logger.java
+++ hdiv-core/src/main/java/org/hdiv/logs/Logger.java
@@ -22,10 +22,9 @@ import org.apache.commons.logging.LogFactory;
 import org.hdiv.util.HDIVUtil;
 
 /**
- * Log that shows the attacks detected by HDIV. It includes type of attack and the
- * identity (application user) of the user. Since the way to obtain this user
- * may vary from application to application, an standard interface has been defined
- * to be implemented by each application.
+ * Log that shows the attacks detected by HDIV. It includes type of attack and the identity (application user) of the
+ * user. Since the way to obtain this user may vary from application to application, an standard interface has been
+ * defined to be implemented by each application.
  * <p>
  * Log format = type;target;parameter;value;userLocalIP;ip;userId
  * </p>
@@ -43,27 +42,62 @@ public class Logger {
 	/**
 	 * Obtains user data from the request
 	 */
-	private IUserData userData;
+	protected IUserData userData;
 
 	/**
 	 * Logger is initialized.
-	 * 
 	 */
 	public void init() {
 	}
 
 	/**
-	 * Prints formatted attack produced by the user if the logging level defined in the Web application rate should be at least INFO.
+	 * Prints formatted attack produced by the user if the logging level defined in the Web application rate should be
+	 * at least INFO.
 	 * 
-	 * @param type Error type
-	 * @param target target name
-	 * @param parameter parameter name
-	 * @param value parameter value
+	 * @param type
+	 *            Error type
+	 * @param target
+	 *            target name
+	 * @param parameter
+	 *            parameter name
+	 * @param value
+	 *            parameter value
 	 */
 	public void log(String type, String target, String parameter, String value) {
-		
-		String formatedData = this.format(type, target, parameter, value);		
+
+		HttpServletRequest request = this.getHttpServletRequest();
+
+		String localIp = this.getUserLocalIP(request);
+		String remoteIp = request.getRemoteAddr();
+		String userName = this.userData.getUsername(request);
+
+		this.log(type, target, parameter, value, localIp, remoteIp, userName);
+	}
+
+	/**
+	 * Logs formatted attack produced by the user.
+	 * 
+	 * @param type
+	 *            Error type
+	 * @param target
+	 *            target name
+	 * @param parameter
+	 *            parameter name
+	 * @param value
+	 *            parameter value
+	 * @param localIp
+	 *            user local ip
+	 * @param remoteIp
+	 *            user remote ip
+	 * @param userName
+	 *            user name in application
+	 */
+	protected void log(String type, String target, String parameter, String value, String localIp, String remoteIp,
+			String userName) {
+
+		String formatedData = this.format(type, target, parameter, value, localIp, remoteIp, userName);
 		log.info(formatedData);
+
 	}
 
 	/**
@@ -71,20 +105,29 @@ public class Logger {
 	 * Formatted text with information from the attack produced by the user. The log format is as follows:
 	 * </p>
 	 * <p>
-	 * <code>[error type];[target];[parameter];[value];[user local IP adress];[IP adress of the client or the last proxy that sent the request];[userId]</code>
+	 * <code>[error type];[target];[parameter];[value];[user local IP address];[IP address of the client or the last proxy that sent the request];[userId]</code>
 	 * </p>
 	 * 
-	 * @param type Error type
-	 * @param target target name
-	 * @param parameter parameter name
-	 * @param value parameter value
+	 * @param type
+	 *            Error type
+	 * @param target
+	 *            target name
+	 * @param parameter
+	 *            parameter name
+	 * @param value
+	 *            parameter value
+	 * @param localIp
+	 *            user local ip
+	 * @param remoteIp
+	 *            user remote ip
+	 * @param userName
+	 *            user name in application
 	 * @return String Formatted text with the attach.
 	 */
-	public String format(String type, String target, String parameter, String value) {
+	protected String format(String type, String target, String parameter, String value, String localIp,
+			String remoteIp, String userName) {
 
-		HttpServletRequest request = this.getHttpServletRequest();
-		
-		StringBuffer buffer = new StringBuffer();	
+		StringBuffer buffer = new StringBuffer();
 		buffer.append(type);
 		buffer.append(";");
 		buffer.append(target);
@@ -93,20 +136,23 @@ public class Logger {
 		buffer.append(";");
 		buffer.append(value);
 		buffer.append(";");
-		buffer.append(this.getUserLocalIP(request));
+		buffer.append(localIp);
 		buffer.append(";");
-		buffer.append(request.getRemoteAddr());
+		buffer.append(remoteIp);
 		buffer.append(";");
-		buffer.append(this.userData.getUsername(request));
-		
+		buffer.append(userName);
+
 		return buffer.toString();
 	}
-	
+
 	/**
-	 * @param request the HttpServletRequest of the request
+	 * Obtain user local ip.
+	 * 
+	 * @param request
+	 *            the HttpServletRequest of the request
 	 * @return Returns the remote user IP address if behind the proxy.
 	 */
-	private String getUserLocalIP(HttpServletRequest request) {
+	protected String getUserLocalIP(HttpServletRequest request) {
 
 		String ipAddress = null;
 
@@ -120,16 +166,13 @@ public class Logger {
 
 	/**
 	 * Obtains the request instance
+	 * 
 	 * @return request
 	 */
-	protected HttpServletRequest getHttpServletRequest(){
-		
+	protected HttpServletRequest getHttpServletRequest() {
+
 		return HDIVUtil.getHttpServletRequest();
-		
-	}
-	
-	public IUserData getUserData() {
-		return userData;
+
 	}
 
 	public void setUserData(IUserData userData) {
