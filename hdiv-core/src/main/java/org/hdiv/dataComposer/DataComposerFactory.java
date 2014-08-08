b25945108fafe25fba9dab6fea2a5812aed978f2
gillarramendi <gotzon@arima.eu>
2014-08-08 10:12:40 +0200

    Don't check if it is an ajax request if it is not necessary

--- hdiv-core/src/main/java/org/hdiv/dataComposer/DataComposerFactory.java
+++ hdiv-core/src/main/java/org/hdiv/dataComposer/DataComposerFactory.java
@@ -144,10 +144,10 @@ public class DataComposerFactory {
 			if (state != null) {
 				dataComposer.beginRequest(state);
 			}
-		} else if (isAjaxRequest(request) && !this.config.isCreateNewPageInAjaxRequest()) {
+		} else if (!this.config.isCreateNewPageInAjaxRequest() && isAjaxRequest(request)) {
 			String hdivStateParamName = (String) request.getSession().getAttribute(Constants.HDIV_PARAMETER);
 			String hdivState = request.getParameter(hdivStateParamName);
-			
+
 			IState state = this.stateUtil.restoreState(hdivState);
 			IPage page = this.session.getPage(Integer.toString(state.getPageId()));
 			dataComposer.startPage(page);
@@ -155,10 +155,11 @@ public class DataComposerFactory {
 			dataComposer.startPage();
 		}
 	}
-	
+
 	protected boolean isAjaxRequest(HttpServletRequest request) {
+
 		String xRequestedWithValue = request.getHeader("x-requested-with");
-		
+
 		return (xRequestedWithValue != null) ? "XMLHttpRequest".equalsIgnoreCase(xRequestedWithValue) : false;
 	}
 
