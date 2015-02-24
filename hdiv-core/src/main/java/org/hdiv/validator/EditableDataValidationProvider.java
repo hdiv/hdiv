117e3b05a4da1efed15d6b5e2670ac3409d4a9d9
gillarramendi <gotzon@arima.eu>
2015-02-24 18:17:39 +0100

    New EditableDataValidationProvider interface

---
+++ hdiv-core/src/main/java/org/hdiv/validator/EditableDataValidationProvider.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2005-2015 hdiv.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hdiv.validator;
+
+public interface EditableDataValidationProvider {
+
+	/**
+	 * <p>
+	 * Checks if the values <code>values</code> are valid for the editable parameter <code>parameter</code>.
+	 * </p>
+	 * 
+	 * @param parameter
+	 *            parameter name
+	 * @param values
+	 *            parameter's values
+	 * @param dataType
+	 *            editable data type
+	 * @return True if the values <code>values</code> are valid for the parameter <code>parameter</code>.
+	 */
+	EditableDataValidationResult validate(String parameter, String[] values, String dataType);
+}
