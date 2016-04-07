4d8c8489e890acada3896e81546da017c0fac871
Ander Ruiz <ander@hdivsecurity.com>
2016-04-07 13:48:04 +0200

    Remove implicit modifiers for interfaces

--- hdiv-core/src/main/java/org/hdiv/config/multipart/IMultipartConfig.java
+++ hdiv-core/src/main/java/org/hdiv/config/multipart/IMultipartConfig.java
@@ -23,7 +23,7 @@ import org.hdiv.filter.RequestWrapper;
 
 /**
  * Class containing multipart request configuration and methods initialized from Spring Factory.
- * 
+ *
  * @author Gorka Vicente
  */
 public interface IMultipartConfig {
@@ -32,25 +32,25 @@ public interface IMultipartConfig {
 	 * This is the ServletRequest attribute that should be set when a multipart request is being read and failed. It's
 	 * the job of the implementation to put this attribute in the request if multipart process failed; in the
 	 * handleRequest(HttpServletRequest) method.
-	 * 
+	 *
 	 * @since HDIV 2.0.1
 	 */
-	public static final String FILEUPLOAD_EXCEPTION = "org.hdiv.exception.HDIVMultipartException";
+	String FILEUPLOAD_EXCEPTION = "org.hdiv.exception.HDIVMultipartException";
 
 	/**
 	 * Parses the input stream and partitions the parsed items into a set of form fields and a set of file items.
-	 * 
+	 *
 	 * @param request The multipart request wrapper.
 	 * @param servletContext Our ServletContext object
 	 * @return multipart processed request
 	 * @throws HdivMultipartException if an unrecoverable error occurs.
 	 */
-	public HttpServletRequest handleMultipartRequest(RequestWrapper request, ServletContext servletContext)
+	HttpServletRequest handleMultipartRequest(RequestWrapper request, ServletContext servletContext)
 			throws HdivMultipartException;
 
 	/**
 	 * Cleanup any resources used for the multipart handling, like a storage for the uploaded files.
-	 * 
+	 *
 	 * @param request the request to cleanup resources for
 	 * @since HDIV 2.1.0
 	 */
