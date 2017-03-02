8d588cc9aec29561dc3875be6a1412eaba5dc2f1
Ander Ruiz <ander@hdivsecurity.com>
2017-03-02 10:20:44 +0100

    Protect non-editable parameter creation with 0 possible values (autosearch)

--- hdiv-core/src/main/java/org/hdiv/state/Parameter.java
+++ hdiv-core/src/main/java/org/hdiv/state/Parameter.java
@@ -74,7 +74,7 @@ public class Parameter implements IParameter {
 
 	public Parameter(final String name, final String[] values, final boolean editable, final String editableDataType,
 			final boolean actionParam) {
-		this(name, values[0], editable, editableDataType, actionParam);
+		this(name, values.length != 0 ? values[0] : null, editable, editableDataType, actionParam);
 		if (values.length > 1) {
 			for (int i = 1; i < values.length; i++) {
 				addValue(values[i]);
