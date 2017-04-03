0f03c9546a0cff14b8d8af28dbca29982ca1d41a
Ander Ruiz <ander@hdivsecurity.com>
2017-04-03 22:00:42 +0200

    Include OPTIONS method

--- hdiv-core/src/main/java/org/hdiv/util/Method.java
+++ hdiv-core/src/main/java/org/hdiv/util/Method.java
@@ -19,7 +19,7 @@ package org.hdiv.util;
  * Class containing valid HTTP Method
  */
 public enum Method {
-	GET(false), HEAD(false), POST(true), PATCH(true), PUT(true), DELETE(true), ANY(false);
+	GET(false), HEAD(false), OPTIONS(false), POST(true), PATCH(true), PUT(true), DELETE(true), ANY(false);
 
 	public final boolean isForm;
 
