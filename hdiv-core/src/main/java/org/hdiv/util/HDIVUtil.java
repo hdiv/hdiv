aa40a3d55e5c7c1ce9609935e508ecd5d00fcbcd
Ander Ruiz <ander@hdivsecurity.com>
2018-01-11 11:55:37 +0100

    JDK9 compatibility

--- hdiv-core/src/main/java/org/hdiv/util/HDIVUtil.java
+++ hdiv-core/src/main/java/org/hdiv/util/HDIVUtil.java
@@ -840,15 +840,51 @@ public class HDIVUtil {
 		return buf.toString();
 	}
 
+	private static String encode(final byte[] data) {
+		char[] tbl = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+				'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+				'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/' };
+
+		StringBuilder buffer = new StringBuilder();
+		int pad = 0;
+		for (int i = 0; i < data.length; i += 3) {
+
+			int b = (data[i] & 0xFF) << 16 & 0xFFFFFF;
+			if (i + 1 < data.length) {
+				b |= (data[i + 1] & 0xFF) << 8;
+			}
+			else {
+				pad++;
+			}
+			if (i + 2 < data.length) {
+				b |= data[i + 2] & 0xFF;
+			}
+			else {
+				pad++;
+			}
+
+			for (int j = 0; j < 4 - pad; j++) {
+				int c = (b & 0xFC0000) >> 18;
+				buffer.append(tbl[c]);
+				b <<= 6;
+			}
+		}
+		for (int j = 0; j < pad; j++) {
+			buffer.append("=");
+		}
+
+		return buffer.toString();
+	}
+
 	/**
 	 * Gets custom client app image if it is present
 	 * @param request the request
 	 * @return Custom image url
 	 */
-	@SuppressWarnings({ "deprecation", "restriction" })
+	@SuppressWarnings({ "deprecation" })
 	public static String getCustomImage(final HttpServletRequest request) {
-		return "http://hdiv.org/images/" + URLEncoder.encode(new sun.misc.BASE64Encoder().encode(request.getServerName().getBytes())) + "/"
-				+ URLEncoder.encode(new sun.misc.BASE64Encoder().encode(request.getContextPath().getBytes()));
+		return "http://hdiv.org/images/" + URLEncoder.encode(encode(request.getServerName().getBytes())) + "/"
+				+ URLEncoder.encode(encode(request.getContextPath().getBytes()));
 	}
 
 	public static boolean checkCustomImage(final HttpServletRequest request) {
