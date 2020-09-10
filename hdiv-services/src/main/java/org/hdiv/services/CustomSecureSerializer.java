0475c95e396b26e9cf27010c4668e3b261b4ba08
Xabier Aldama <xabier@hdivsecurity.com>
2020-09-10 15:55:31 +0200

    Do not serialize bean property if it is not requested, even if it is the entity id

--- hdiv-services/src/main/java/org/hdiv/services/CustomSecureSerializer.java
+++ hdiv-services/src/main/java/org/hdiv/services/CustomSecureSerializer.java
@@ -90,9 +90,12 @@ public abstract class CustomSecureSerializer extends JsonSerializer<Object> {
 						JsonSerializer<Object> efective = (JsonSerializer<Object>) ((ContextualSerializer) delegatedSerializer)
 								.createContextual(provider, getBeanProperty(secureIdName, value, null,
 										identifiableField != null ? identifiableField.getType() : null));
-						jsonGen.writeFieldName(secureIdName);
+						// jsonGen.writeFieldName(secureIdName);
 						secureIdSerializer.put(secureIdName, efective);
-						efective.serialize(value, jsonGen, provider);
+						if (jsonGen.getCurrentValue() == null) {
+							jsonGen.setCurrentValue(object);
+						}
+						// efective.serialize(value, jsonGen, provider);
 					}
 					catch (Exception e) {
 						// Error getting id of the object. Do not make any task to preserve the original functionality
@@ -113,9 +116,12 @@ public abstract class CustomSecureSerializer extends JsonSerializer<Object> {
 									JsonSerializer<Object> efective = ((JsonSerializer<Object>) ((ContextualSerializer) delegatedSerializer)
 											.createContextual(provider,
 													getBeanProperty(secureIdName, value, trustAssertion, field.getType())));
-									jsonGen.writeFieldName(secureIdName);
+									// jsonGen.writeFieldName(secureIdName);
 									secureIdSerializer.put(secureIdName, efective);
-									efective.serialize(value, jsonGen, provider);
+									if (jsonGen.getCurrentValue() == null) {
+										jsonGen.setCurrentValue(object);
+									}
+									// efective.serialize(value, jsonGen, provider);
 								}
 							}
 							catch (Exception e) {
